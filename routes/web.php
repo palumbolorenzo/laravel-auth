57fbd8947512aa6777f886f6b84daadbefff43c6
palumbolorenzo <palumbolorenzo97@gmail.com>
2023-01-27 17:20:41 +0100

    provvisorio

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'Admin\HomeController@index')->name('home');
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function (){
+        Route::get('/home', 'HomeController@index')
+        ->name('home');
+   });
